ae2aed4ef0dbfaefbdd1a404150ca3b82bd4edf6
angelxtry <angelxtry@gmail.com>
2023-07-23 20:10:34 +0900

    ✨ 잡다한 수정 1

--- hello-world.js
+++ hello-world.js
@@ -1,20 +1,15 @@
 console.log('major update');
 
 console.log('feature-2');
-
 console.log('feature-3-2');
-
 console.log('feature-4-1');
-
 console.log('feature-5-1');
-
 console.log('feature-5-2');
-
 console.log('feature-5-3');
-
 console.log('feature-6');
 console.log('feature-7');
 
 console.log('fix-1-2');
 
 console.log('chore-1-1');
+console.log('chore-test-1');
