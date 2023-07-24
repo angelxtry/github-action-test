08ebef5943da4b2fd36f0d629e833a48c6470d2c
joker <angelxtry@gmail.com>
2023-07-24 10:46:36 +0900

    깃모지 fix 수정 1번 추가

--- hello-world.js
+++ hello-world.js
@@ -3,7 +3,7 @@ console.log('major update');
 console.log('feature-12');
 console.log('task-1');
 
-console.log('fix-1-2');
+console.log('fix-1');
 
 console.log('chore-1-1');
 console.log('chore-test-1');
